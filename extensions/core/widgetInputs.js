f4d17b2022504bfcd7d49078c477a580f168df1d
MRNF <mrnfqrbl@gmail.com>
2025-03-13 01:30:35 +0800

    1

--- extensions/core/widgetInputs.js
+++ extensions/core/widgetInputs.js
@@ -1,4 +1,5 @@
 // Shim for extensions/core/widgetInputs.ts
+export const PrimitiveNode = window.comfyAPI.widgetInputs.PrimitiveNode;
 export const getWidgetConfig = window.comfyAPI.widgetInputs.getWidgetConfig;
 export const convertToInput = window.comfyAPI.widgetInputs.convertToInput;
 export const setWidgetConfig = window.comfyAPI.widgetInputs.setWidgetConfig;
